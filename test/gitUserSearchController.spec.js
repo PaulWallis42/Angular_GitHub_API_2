c624d643688cb616fa5ec7480ff96b0a5ab2a744
paulwallis42 <paulwallis42@gmail.com>
2015-04-28 15:40:07 +0100

    Two passing tests but without the gh api token

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -12,25 +12,36 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
-describe('when searching for a user', function() {
+  describe('when searching for a user', function() {
 
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+        { items: items }
+      );
+    }));
 
-  it('displays search results', function() {
-    ctrl.searchTerm = 'hello';
-    ctrl.doSearch();
-    expect(ctrl.searchResult.items).toEqual(items);
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
-});
 });
