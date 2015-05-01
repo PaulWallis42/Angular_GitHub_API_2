c238c71bff1aab4e7d618de1046cc72d2529656e
paulwallis42 <paulwallis42@gmail.com>
2015-05-01 08:22:00 +0100

    test changed to include an array iteration - Test Pass

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -12,9 +12,11 @@ describe('GitHub profile finder', function() {
   });
 
   it('finds profiles', function() {
-    searchBox.sendKeys('spike01');
-    searchButton.click();
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
-  });
 
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.getText()).toContain('spike01');
+  });
 });
